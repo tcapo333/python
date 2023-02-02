71e068d99392e3d74569a94937b4411f9a57b302
unknown <2btd05@IESRAMONLLULL.LOCAL>
2023-02-02 13:06:45 +0100

    acabado

--- tres.py
+++ tres.py
@@ -1,27 +1,92 @@
 import random
 casillas=[' ',' ',' ',' ',' ',' ',' ',' ',' ']
-otra=true
+lista=[0,1,2,3,4,5,6,7,8]
+seguir = True
+turnos = 9
 print('''
     | | | |
     | | | |
     | | | |
     ''')
-while seguir == true :
-    while otra == true
-        posicion=int(input('elige una casilla del 0 al 8: '))
-        if casillas[posicion]==' '
-            casillas[posicion]='X'
-            otra=false
-            print(
-                '|'+casillas[0]+'|'+casillas[1]+'|'+casillas[2]+'|\n' 
-                '|'+casillas[3]+'|'+casillas[4]+'|'+casillas[5]+'|\n'
-                '|'+casillas[6]+'|'+casillas[7]+'|'+casillas[8]+'|\n'    
-                )
-        else posicion=int(input('elige una casilla del 0 al 8: '))
-            otra=true
 
-    posicion=random.randint(0,8)
+while seguir == True :
+    
+    posicion=int(input('elige una casilla del 0 al 8: '))
+    if casillas[posicion] == ' ' :
+     casillas[posicion]='X'
+     lista.remove(posicion)
+     print('|'+casillas[0]+'|'+casillas[1]+'|'+casillas[2]+'|\n' '|'+casillas[3]+'|'+casillas[4]+'|'+casillas[5]+'|\n' '|'+casillas[6]+'|'+casillas[7]+'|'+casillas[8]+'|\n')
+    else:  
+        posicion=int(input('elige una casilla que no este ocpuada del 0 al 8: ')) 
+        casillas[posicion]='X'
+        lista.remove(posicion)
 
+    if casillas[0]==casillas[1]==casillas[2]=='X' :
+        print('has ganado')
+        seguir=False
+        break
+    if casillas[0]==casillas[1]==casillas[2]=='O' :
+        print('has perdido')
+        seguir=False
+        break
+    if casillas[3]==casillas[4]==casillas[5]=='X' :
+        print('has ganado')
+        seguir=False
+        break
+    if casillas[3]==casillas[4]==casillas[5]=='O' :
+        print('has perdido')
+        seguir=False
+        break
+    if casillas[6]==casillas[7]==casillas[8]=='X':
+        print('has ganado')
+        seguir=False
+        break
+    if casillas[6]==casillas[7]==casillas[8]=='O' :
+        print('has perdido')
+        seguir=False
+        break
+    if casillas[0]==casillas[3]==casillas[6]=='X' :
+        print('has ganado')
+        seguir=False
+        break
+    if casillas[0]==casillas[3]==casillas[6]=='O' :
+        print('has perdido')
+        seguir=False
+        break
+    if casillas[1]==casillas[4]==casillas[7]=='X' :
+        print('has ganado')
+        seguir=False
+        break
+    if casillas[1]==casillas[4]==casillas[7]=='O' :
+        print('has perdido')
+        seguir=False
+        break
+    if casillas[2]==casillas[5]==casillas[8]=='X' :
+        print('has ganado')
+        seguir=False
+        break
+    if casillas[2]==casillas[5]==casillas[8]=='O' :
+        print('has perdido')
+        seguir=False
+        break
+    if casillas[0]==casillas[4]==casillas[8]=='X' :
+        print('has ganado')
+        seguir=False
+        break
+    if casillas[0]==casillas[4]==casillas[8]=='O' :
+        print('has perdido')
+        seguir=False
+        break
+    if casillas[2]==casillas[4]==casillas[6]=='X' :
+        print('has ganado')
+        seguir=False
+        break
+    turnos=turnos-1
+    if turnos == 0:
+        break
+
+    posicion=random.choice(lista)
+    lista.remove(posicion)
     casillas[posicion]='O'
     print(
         '|'+casillas[0]+'|'+casillas[1]+'|'+casillas[2]+'|\n' 
@@ -29,48 +94,69 @@ while seguir == true :
         '|'+casillas[6]+'|'+casillas[7]+'|'+casillas[8]+'|\n'    
         )
 
-    if casillas[0]==casillas[1]==casillas[2]=='X'
+    if casillas[0]==casillas[1]==casillas[2]=='X' :
         print('has ganado')
-        seguir=false
-    if casillas[0]==casillas[1]==casillas[2]=='O'
+        seguir=False
+        break
+    if casillas[0]==casillas[1]==casillas[2]=='O' :
         print('has perdido')
-        seguir=false
-    if casillas[3]==casillas[4]==casillas[5]=='X'
+        seguir=False
+        break
+    if casillas[3]==casillas[4]==casillas[5]=='X' :
         print('has ganado')
-        seguir=false
-    if casillas[3]==casillas[4]==casillas[5]=='O'
+        seguir=False
+        break
+    if casillas[3]==casillas[4]==casillas[5]=='O' :
         print('has perdido')
-        seguir=false
-    if casillas[6]==casillas[7]==casillas[8]=='X'
+        seguir=False
+        break
+    if casillas[6]==casillas[7]==casillas[8]=='X':
         print('has ganado')
-        seguir=false
-    if casillas[6]==casillas[7]==casillas[8]=='O'
+        seguir=False
+        break
+    if casillas[6]==casillas[7]==casillas[8]=='O' :
         print('has perdido')
-        seguir=false
-    if casillas[0]==casillas[3]==casillas[6]=='X'
+        seguir=False
+        break
+    if casillas[0]==casillas[3]==casillas[6]=='X' :
         print('has ganado')
-        seguir=false
-    if casillas[0]==casillas[3]==casillas[6]=='O'
+        seguir=False
+        break
+    if casillas[0]==casillas[3]==casillas[6]=='O' :
         print('has perdido')
-        seguir=false
-    if casillas[1]==casillas[4]==casillas[7]=='X'
+        seguir=False
+        break
+    if casillas[1]==casillas[4]==casillas[7]=='X' :
         print('has ganado')
-        seguir=false
-    if casillas[1]==casillas[4]==casillas[7]=='O'
+        seguir=False
+        break
+    if casillas[1]==casillas[4]==casillas[7]=='O' :
         print('has perdido')
-        seguir=false
-    if casillas[2]==casillas[5]==casillas[8]=='X'
+        seguir=False
+        break
+    if casillas[2]==casillas[5]==casillas[8]=='X' :
         print('has ganado')
-        seguir=false
-    if casillas[2]==casillas[5]==casillas[8]=='O'
+        seguir=False
+        break
+    if casillas[2]==casillas[5]==casillas[8]=='O' :
         print('has perdido')
-        seguir=false
-    if casillas[0]==casillas[4]==casillas[8]=='X'
+        seguir=False
+        break
+    if casillas[0]==casillas[4]==casillas[8]=='X' :
         print('has ganado')
-        seguir=false
-    if casillas[0]==casillas[4]==casillas[8]=='O'
+        seguir=False
+        break
+    if casillas[0]==casillas[4]==casillas[8]=='O' :
         print('has perdido')
-        seguir=false
-    if casillas[2]==casillas[4]==casillas[6]=='X'
+        seguir=False
+        break
+    if casillas[2]==casillas[4]==casillas[6]=='X' :
         print('has ganado')
-        seguir=false
+        seguir=False
+        break
+    
+    turnos=turnos-1
+    if turnos == 0: 
+        break
+        print('has empatado')
+        
